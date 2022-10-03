8ddd7a72c8ab3163cc430ae03d59113ccab9fbe8
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-10-03 16:03:51 +0800

    refactor to adapt to use-package config.

--- ghcid.el
+++ ghcid.el
@@ -242,28 +242,13 @@ To configure where the new buffer should appear, customize your
 If the window that shows ghcid changes size, the process will not
 recognize the new height until you manually restart it by calling
 `ghcid' again."
-  :lighter " Ghcid"
+  :lighter " Ghcid")
+
+(defun ghcid-activate-mode ()
+  "Really activate the ghcid mode."
   (when (fboundp 'nlinum-mode) (nlinum-mode -1))
   (linum-mode -1)
   (compilation-minor-mode))
-
-
-;; Compilation mode does some caching for markers in files, but it gets confused
-;; because ghcid reloads the files in the same process. Here we parse the
-;; 'Reloading...' message from ghcid and flush the cache for the mentioned
-;; files. This approach is very similar to the 'omake' hacks included in
-;; compilation mode.
-(add-to-list
-  'compilation-error-regexp-alist-alist
-  '(ghcid-reloading
-    "Reloading\\.\\.\\.\\(\\(\n  .+\\)*\\)" 1 nil nil nil nil
-    (0 (progn
-         (let* ((filenames (cdr (split-string (match-string 1) "\n  "))))
-           (dolist (filename filenames)
-             (compilation--flush-file-structure filename)))
-         nil))))
-(add-to-list 'compilation-error-regexp-alist 'ghcid-reloading)
-
 
 (defun ghcid-buffer-name ()
   "Construct a ghcid buffer name."
@@ -282,23 +267,27 @@ with the specified CMD, TESTCMD, SETUPCMD, LINTCMD and H."
           cmd testcmd setupcmd lintcmd h))
 
 (defun ghcid-get-buffer ()
+  "Create or reuse a ghcid buffer with the configured name."
+  (get-buffer-create (ghcid-buffer-name)))
+
+(defun ghcid-get-buffer-display ()
   "Create or reuse a ghcid buffer with the configured name and display it.
 Return the window that show the buffer.
 
 User configuration will influence where the buffer gets shown
 exactly. See `ghcid-mode'."
-  (display-buffer (get-buffer-create (ghcid-buffer-name)) '((display-buffer-reuse-window                ;; first, try to reuse existing window
-                                                             display-buffer-in-side-window)             ;; otherwise, create in side window
-                                                            (side . right)                              ;; at right side
-                                                            (slot . 0)                                  ;; slot 0
-                                                            (window-width . fit-window-to-buffer)       ;; fit window width to buffer
-                                                            )))
+  (display-buffer (ghcid-get-buffer) '((display-buffer-reuse-window                ;; first, try to reuse existing window
+                                        display-buffer-in-side-window)             ;; otherwise, create in side window
+                                       (side . right)                              ;; at right side
+                                       (slot . 0)                                  ;; slot 0
+                                       (window-width . fit-window-to-buffer)       ;; fit window width to buffer
+                                       )))
 
 (defun ghcid-start (dir cmd testcmd setupcmd lintcmd)
   "Start ghcid.
 in the specified directory DIR and CMD, TESTCMD, SETUPCMD and LINTCMD."
 
-  (with-selected-window (ghcid-get-buffer)
+  (with-selected-window (ghcid-get-buffer-display)
 
     (setq next-error-last-buffer (current-buffer))
     (setq-local default-directory dir)
@@ -308,7 +297,23 @@ in the specified directory DIR and CMD, TESTCMD, SETUPCMD and LINTCMD."
 
       (term-mode)
       (term-line-mode)  ;; Allows easy navigation through the buffer
-      ;;(ghcid-mode)
+      (ghcid-activate-mode) ;; This will active the compilation-mode
+
+      ;; Compilation mode does some caching for markers in files, but it gets confused
+      ;; because ghcid reloads the files in the same process. Here we parse the
+      ;; 'Reloading...' message from ghcid and flush the cache for the mentioned
+      ;; files. This approach is very similar to the 'omake' hacks included in
+      ;; compilation mode.
+      (add-to-list
+       'compilation-error-regexp-alist-alist
+       '(ghcid-reloading
+         "Reloading\\.\\.\\.\\(\\(\n  .+\\)*\\)" 1 nil nil nil nil
+         (0 (progn
+              (let* ((filenames (cdr (split-string (match-string 1) "\n  "))))
+                (dolist (filename filenames)
+                  (compilation--flush-file-structure filename)))
+              nil))))
+      (add-to-list 'compilation-error-regexp-alist 'ghcid-reloading)
 
       (setq-local term-buffer-maximum-size height)
       (setq-local scroll-up-aggressively 1)
